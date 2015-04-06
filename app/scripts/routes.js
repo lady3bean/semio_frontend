b5bf5c971a9e580105eb7ed893564a1150a36ca8
lady3bean <annehawleybrett@gmail.com>
2015-04-06 18:39:34 -0400

    moves routes to its own sensible file

---
+++ app/scripts/routes.js
@@ -0,0 +1,26 @@
+angular
+  .module('semio', [
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .when('/contact', {
+        templateUrl: 'views/contact.html',
+        controller: 'ContactCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
