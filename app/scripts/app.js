10ead989f59128623e3feb946c8a807161e85786
lady3bean <annehawleybrett@gmail.com>
2015-04-06 18:46:56 -0400

    initial setup of the main module with the constant set for the API URL, and reference to a not-yet-existing WordFactory

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,36 +1,13 @@
-'use strict';
+(function() {
+    'use strict';
 
-/**
- * @ngdoc overview
- * @name semioFrontendApp
- * @description
- * # semioFrontendApp
- *
- * Main module of the application.
- */
-angular
-  .module('semio', [
-    'ngCookies',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl'
-      })
-      .when('/contact', {
-        templateUrl: 'views/contact.html',
-        controller: 'ContactCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+    angular
+        .module('Semio', [])
+        .run(function(WordFactory) {
+            WordFactory.getWords();
+        });
+
+    angular.module('WordManager')
+        .constant('ServerUrl', 'http://localhost:3000');
+
+})();
