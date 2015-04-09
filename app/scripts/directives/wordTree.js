f95fe7fed056a291ee91ee1f210f6cfe70fa2b8e
lady3bean <annehawleybrett@gmail.com>
2015-04-09 15:13:48 -0400

    introduces d3 model and directive in place of the old word-tree directive. Includes practice d3 code to see if it's working, not the final word-tree

--- app/scripts/directives/wordTree.js
+++ app/scripts/directives/wordTree.js
@@ -3,19 +3,85 @@
 
   angular
     .module('Semio')
-    .directive('wordTree', wordTree);
+    .directive('wordTree', ['d3Service', function( d3Service ) {
 
-    function wordTree() {
       return {
-          restrict: 'E',
-          templateUrl: 'templates/wordTree.html',
-          scope: {
-              words: '='
-          },
-          controller:  ContentCtrl,
-          controllerAs: 'contentCtrl',
-          bindToController: true
-      };
-    }
+        restrict: 'EA',
+        scope: {},
+        template: '<div class="word-tree"></div>',
+        // templateUrl: 'views/wordTree.html',
+        // scope: { words: '=' },
+        // controller:  ContentCtrl,
+        // controllerAs: 'contentCtrl',
+        // bindToController: true,
+        link: function( scope, element, attrs ) {
+          d3Service.d3().then(function(d3) {
+            var margin = parseInt(attrs.margin) || 20;
+            var barHeight = parseInt(attrs.barHeight) || 20;
+            var barPadding = parseInt(attrs.barPadding) || 5;
 
+            var svg = d3.select(element[0])
+                        .append("svg")
+                        .style('width', '100%');
+
+            window.onresize = function() {
+              scope.$apply();
+            };
+
+            scope.data = [
+              {name: "Greg", score: 98},
+              {name: "Ari", score: 96},
+              {name: 'Q', score: 75},
+              {name: "Loser", score: 48}
+            ];
+
+            scope.$watch(function() {
+              return angular.element($window)[0].innerWidth;
+            }, function() {
+              scope.render(scope.data);
+            });
+
+            scope.render = function(data) {
+              svg.selectAll('*').remove();
+
+              // If we don't pass any data, return out of the element
+              if (!data) return;
+
+              // setup variables
+              var width = d3.select(element[0]).node().offsetWidth - margin,
+                  // calculate the height
+                  height = scope.data.length * (barHeight + barPadding),
+                  // Use the category20() scale function for multicolor support
+                  color = d3.scale.category20(),
+                  // our xScale
+                  xScale = d3.scale.linear()
+                    .domain([0, d3.max(data, function(d) {
+                      return d.score;
+                    })])
+                    .range([0, width]);
+
+                  // set the height based on the calculations above
+                  svg.attr('height', height);
+
+                  //create the rectangles for the bar chart
+                  svg.selectAll('rect')
+                    .data(data).enter()
+                      .append('rect')
+                      .attr('height', barHeight)
+                      .attr('width', 140)
+                      .attr('x', Math.round(margin/2))
+                      .attr('y', function(d,i) {
+                        return i * (barHeight + barPadding);
+                      })
+                      .attr('fill', function(d) { return color(d.score); })
+                      .transition()
+                        .duration(1000)
+                        .attr('width', function(d) {
+                          return xScale(d.score);
+                        });
+            };
+          });
+        }  
+      };
+    }]);
 })();
