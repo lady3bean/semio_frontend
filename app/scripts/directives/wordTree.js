e0d4dea50cb534a96c1be24dccc85e5848b83044
lady3bean <annehawleybrett@gmail.com>
2015-04-09 15:56:41 -0400

    clears out dummy d3 example, passes $window into the word-tree directive

--- app/scripts/directives/wordTree.js
+++ app/scripts/directives/wordTree.js
@@ -9,16 +9,9 @@
         restrict: 'EA',
         scope: {},
         template: '<div class="word-tree"></div>',
-        // templateUrl: 'views/wordTree.html',
-        // scope: { words: '=' },
-        // controller:  ContentCtrl,
-        // controllerAs: 'contentCtrl',
-        // bindToController: true,
-        link: function( scope, element, attrs ) {
+  
+        link: function( scope, element, attrs, $window ) {
           d3Service.d3().then(function(d3) {
-            var margin = parseInt(attrs.margin) || 20;
-            var barHeight = parseInt(attrs.barHeight) || 20;
-            var barPadding = parseInt(attrs.barPadding) || 5;
 
             var svg = d3.select(element[0])
                         .append("svg")
@@ -44,41 +37,8 @@
             scope.render = function(data) {
               svg.selectAll('*').remove();
 
-              // If we don't pass any data, return out of the element
               if (!data) return;
 
-              // setup variables
-              var width = d3.select(element[0]).node().offsetWidth - margin,
-                  // calculate the height
-                  height = scope.data.length * (barHeight + barPadding),
-                  // Use the category20() scale function for multicolor support
-                  color = d3.scale.category20(),
-                  // our xScale
-                  xScale = d3.scale.linear()
-                    .domain([0, d3.max(data, function(d) {
-                      return d.score;
-                    })])
-                    .range([0, width]);
-
-                  // set the height based on the calculations above
-                  svg.attr('height', height);
-
-                  //create the rectangles for the bar chart
-                  svg.selectAll('rect')
-                    .data(data).enter()
-                      .append('rect')
-                      .attr('height', barHeight)
-                      .attr('width', 140)
-                      .attr('x', Math.round(margin/2))
-                      .attr('y', function(d,i) {
-                        return i * (barHeight + barPadding);
-                      })
-                      .attr('fill', function(d) { return color(d.score); })
-                      .transition()
-                        .duration(1000)
-                        .attr('width', function(d) {
-                          return xScale(d.score);
-                        });
             };
           });
         }  
