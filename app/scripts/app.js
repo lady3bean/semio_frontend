22c61bea38b2aa22521c42af07fd7b5d9c701b3e
lady3bean <annehawleybrett@gmail.com>
2015-04-16 14:50:48 -0400

    loading in d3 from a cdn to speed up the request and use https

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -31,7 +31,7 @@
     var scriptTag = $document[0].createElement('script');
     scriptTag.type = 'text/javascript';
     scriptTag.async = true;
-    scriptTag.src = 'https://d3js.org/d3.v3.min.js';
+    scriptTag.src = 'https://cdnjs.cloudflare.com/ajax/libs/d3/3.5.5/d3.min.js';
 
     scriptTag.onreadystatechange = function() {
       if (this.readyState === 'complete') {
