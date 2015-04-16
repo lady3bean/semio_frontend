b64c07e4b6bdf7cae48ee423eec11fbbb9f746c8
lady3bean <annehawleybrett@gmail.com>
2015-04-16 14:37:39 -0400

    specifies https due to error: Mixed Content: The page at 'https://semio.herokuapp.com/' was loaded over HTTPS, but requested an insecure script 'http://d3js.org/d3.v3.min.js'. This request has been blocked; the content must be served over HTTPS.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -31,7 +31,7 @@
     var scriptTag = $document[0].createElement('script');
     scriptTag.type = 'text/javascript';
     scriptTag.async = true;
-    scriptTag.src = 'http://d3js.org/d3.v3.min.js';
+    scriptTag.src = 'https://d3js.org/d3.v3.min.js';
 
     scriptTag.onreadystatechange = function() {
       if (this.readyState === 'complete') {
